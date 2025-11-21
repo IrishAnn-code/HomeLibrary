ecfff8f7286bba312a88c21518e015d9368ad982
IrishAnn <irishann.tr@gmail.com>
2025-11-21 11:24:33 +0900

    add: Добавлены жанры и доступ к русской раскладке статуса чтения

--- app/models/enum.py
+++ app/models/enum.py
@@ -6,8 +6,47 @@ class ReadStatus(str, Enum):
     READING = "reading"
     READ = "read"
 
+    @property
+    def russian_name(self):
+        names = {
+            ReadStatus.NOT_READ: "Не прочитано",
+            ReadStatus.READING: "Читаю",
+            ReadStatus.READ: "Прочитано"
+        }
+        return names[self]
+
 
 class LibraryRole(str, Enum):
     OWNER = "owner"
     MEMBER = "member"
     GUEST = "guest"
+
+
+class GenreStatus(str, Enum):
+    ADVENTURE = "adventure"
+    CHILDREN_S = "children's"
+    CLASSICS = "classics"
+    DETECTIVE = "detective"
+    DRAMA = "drama"
+    FANTASY = "fantasy"
+    NOVEL = "novel"
+    POETRY = "poetry"
+    SCIENCE_FICTION = "science_fiction"
+    OTHER = "other"
+
+    @property
+    def russian_name(self):
+        names = {
+            GenreStatus.DETECTIVE: "детектив",
+            GenreStatus.CHILDREN_S: "детская литература",
+            GenreStatus.DRAMA: "драма",
+            GenreStatus.CLASSICS: "классика",
+            GenreStatus.SCIENCE_FICTION: "научная фантастика",
+            GenreStatus.POETRY: "поэзия",
+            GenreStatus.ADVENTURE: "приключения",
+            GenreStatus.NOVEL: "роман",
+            GenreStatus.FANTASY: "фэнтези",
+            GenreStatus.OTHER: "другое"
+        }
+        return names[self]
+
