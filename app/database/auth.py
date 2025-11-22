4d174fcd74efc48ae36e26d114ebad506e9c7e49
IrishAnn <irishann.tr@gmail.com>
2025-11-22 20:57:17 +0900

    refactor: Упрощение запроса в БД

--- app/database/auth.py
+++ app/database/auth.py
@@ -94,8 +94,7 @@ async def get_current_user(
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-    result = await db.execute(select(User).where(User.id == user_id))
-    user = result.scalar_one_or_none()
+    user = await db.scalar(select(User).where(User.id == user_id))
 
     if not user:
         logger.warning(f"❌ User {user_id} not found in database")
